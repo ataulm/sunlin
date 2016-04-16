21edcce475e5ad5a7fe25dd92581bd1d81cadf3b
Ataul Munim <hello@ataulm.com>
2016-04-16 19:03:52 +0100

    Create dummy data

--- mobile/src/main/java/com/ataulm/basic/MyActivity.kt
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.kt
@@ -8,6 +8,19 @@ class MyActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
+
+        val dummyData = createDummyData()
+    }
+
+    private fun createDummyData() : List<String> {
+        return listOf(
+                "Today - Sunny - 88 / 63",
+                "Tomorrow - Foggy - 70 / 46",
+                "Weds - Cloudy - 72 / 63",
+                "Thurs - Rainy - 64 / 51",
+                "Fri - Foggy - 70 / 46",
+                "Sat - Sunny - 76 / 68"
+        )
     }
 
 }
