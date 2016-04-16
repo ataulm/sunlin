e64aa82a55ae6721f1ef400afcfdfa15efcfad3b
Ataul Munim <hello@ataulm.com>
2016-04-16 19:20:55 +0100

    Add dummy data to listview

--- mobile/src/main/java/com/ataulm/basic/MyActivity.kt
+++ mobile/src/main/java/com/ataulm/basic/MyActivity.kt
@@ -2,6 +2,12 @@ package com.ataulm.basic
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class MyActivity : AppCompatActivity() {
 
@@ -9,10 +15,12 @@ class MyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my)
 
+        val listView = findViewById(R.id.listview) as ListView
         val dummyData = createDummyData()
+        listView.setAdapter(WeatherAdapter(layoutInflater, dummyData))
     }
 
-    private fun createDummyData() : List<String> {
+    private fun createDummyData(): List<String> {
         return listOf(
                 "Today - Sunny - 88 / 63",
                 "Tomorrow - Foggy - 70 / 46",
@@ -21,6 +29,44 @@ class MyActivity : AppCompatActivity() {
                 "Fri - Foggy - 70 / 46",
                 "Sat - Sunny - 76 / 68"
         )
+    }
+
+    private class WeatherAdapter(
+            private val layoutInflater: LayoutInflater,
+            private val data: List<String>
+    ) : BaseAdapter() {
+
+        override fun getCount(): Int {
+            return data.size
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var view = convertView
+            if (view == null) {
+                view = createView(parent)
+            }
+            bindView(view, position)
+            return view
+        }
+
+        private fun createView(parent: ViewGroup): View {
+            val view = layoutInflater.inflate(R.layout.list_item_forecast, parent, false)
+            return view
+        }
+
+        private fun bindView(view: View, position: Int) {
+            val textView = view as TextView
+            textView.text = getItem(position)
+        }
+
+        override fun getItem(position: Int): String {
+            return data.get(position)
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
     }
 
 }
